7ad1e94ce12dac1e75cf2f6c5996fc645068a214
mthpnts <14241129+mthpnts@users.noreply.github.com>
2024-11-17 20:22:05 -0300

    delete
    delete

--- EventHub-backend/Event/Home/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from Home.models import Event
-
-
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('title', 'slug', 'venue', 'organizer', 'date',
-                  'time', 'fee', 'interested', 'imagePath')
--- EventHub-backend/Event/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("", views.profile, name="profile"),
-    path("profile/<str:name>", views.profile, name="profile"),
-    path("signup/", views.signup, name="signup"),
-]
